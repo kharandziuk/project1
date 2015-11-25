2835b4a9207460e6300185bb8655816dc70a65fe
Max Kharandziuk <max.kharandziuk@gmail.com>
2015-11-25 14:21:46 +0200

    initial implementation

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
 const H = require('highland')
 const P = require('bluebird')
-const R = P.promisify(require('request'))
+const promisify = P.promisify
 const _ = require('underscore')
 const cors = require('cors')
 const express = require('express')
+const assert = require('assert');
+const bodyParser = require('body-parser')
 
 const PORT = 8000
+var {MongoClient, ObjectID} = P.promisifyAll(require('mongodb'))
+var url = 'mongodb://localhost:8017/local';
+
 
 const app = express()
+
 app.use(cors())
+app.use(bodyParser.json())
+
+app.use(function(req, res, next){
+  var db = MongoClient.connectAsync(url)
+  req.db = (...names) => {
+      return db.then((db)=> names.map(n => db.collection(n)))
+  }
+  next()
+})
 
 app.get('/pretendents', (req, res)=> {
-  res.json([
-    { id: 1, firstName: 'Max', lastName: 'Kharandziuk'},
-    { id: 2, firstName: 'Olga', lastName: 'Yaksun'},
-    { id: 3, firstName: 'Katia', lastName: 'Bukina'},
-  ])
+  req.db('pretendents')
+    .spread((pretendents) => pretendents.find({}).toArrayAsync())
+    .then((docs) =>  res.json(docs))
 })
+
+app.post('/pretendents', (req, res)=> {
+  req.db('pretendents')
+    .spread((pretendents) => 
+      pretendents.insertAsync(
+        {firstName: '', lastName: ''}
+      )
+    )
+    .then(({ops}) => {
+      res.json(ops[0])
+    })
+})
+
+app.put('/pretendents', (req, res)=> {
+  let id = req.body._id
+  req.db('pretendents')
+    .spread((pretendents) =>
+        pretendents.updateOneAsync({_id: ObjectID(id)}, _(req.body).omit('_id'))
+    )
+    .then((docs)=> res.json({'_id': id}))
+})
+
 app.listen(PORT, function() {
   console.log(`listen on port ${PORT}`)
 })
