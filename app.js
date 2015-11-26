18a631b6e87e68fc0fe45cf444f0da39b4f2f629
Max Kharandziuk <max.kharandziuk@gmail.com>
2015-11-26 18:54:00 +0200

    added ability to upload attachments

--- app.js
+++ app.js
@@ -6,29 +6,90 @@ const cors = require('cors')
 const express = require('express')
 const assert = require('assert');
 const bodyParser = require('body-parser')
+const busboy = require('connect-busboy')
+const path = require('path')
+const uuid = require('node-uuid')
+const fs = require('fs')
 
 const PORT = 8000
 var {MongoClient, ObjectID} = P.promisifyAll(require('mongodb'))
 var url = 'mongodb://localhost:8017/local';
 
 
+let mediaPath = path.resolve(__dirname, 'media')
+
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(busboy())
+
+
 
 app.use(function(req, res, next){
-  var db = MongoClient.connectAsync(url)
-  req.db = (...names) => {
-      return db.then((db)=> names.map(n => db.collection(n)))
-  }
+  req.db = name =>
+    MongoClient.connectAsync(url).then((db) => db.collection(name))
   next()
 })
 
 app.get('/pretendents', (req, res)=> {
   req.db('pretendents')
-    .spread((pretendents) => pretendents.find({}).toArrayAsync())
-    .then((docs) =>  res.json(docs))
+    .then((pretendents) => pretendents.find({}).toArrayAsync())
+    .then((docs) => res.json(docs))
+})
+
+app.post('/attachments',
+    (req, res, next)=> {
+      let {busboy} = req
+      assert(!_.isUndefined(busboy))
+      req.pipe(busboy)
+      let parserStream = H(
+        function(push) {
+          busboy.on('file', (fieldname, file, filename, encoding, mimetype)=> {
+            let name = uuid.v1()
+            let extension = path.extname(filename)
+            let saveTo = path.resolve(mediaPath, `${name}${extension}`)
+            file.pipe(fs.createWriteStream(saveTo))
+            push(null, {
+              [fieldname]: { filename }
+            })
+          })
+          busboy.on('field', (key, value, keyTruncated, valueTruncated) => {
+            push(null, {[key]: value})
+          })
+          busboy.on('finish', () =>
+            push(null, H.nil)
+          )
+        })
+        .reduce({}, _.extend)
+
+      req.data = new Promise((resolve) => {
+        parserStream.apply(resolve)
+      })
+      next()
+    },
+    (req, res)=> {
+      Promise.all([req.data, req.db('pretendents')])
+        .then(([data, pretendents]) => {
+          return Promise.all([
+            pretendents.find(ObjectID(data.pretendentId)).toArrayAsync().then((docs) => { return docs.length === 1}),
+            data,
+            req.db('attachments'),
+          ])
+        })
+        .then(([isUser, data, attachments])=> {
+            assert(isUser)
+            console.log(data)
+            return attachments.insertAsync(data)
+        })
+        .then(({ops})=> {
+            console.log('added an attachment')
+            res.json(ops[0])
+        })
+        .catch((err)=> {
+          console.log('some error')
+          console.log(err)
+        })
 })
 
 app.post('/pretendents', (req, res)=> {
