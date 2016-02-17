e18fe607f67d676fb4ba1034a60c419703013ab5
Max Kharandziuk <max.kharandziuk@gmail.com>
2016-02-17 11:34:58 +0200

    added port environment variable

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const {APP_PORT} = process.env
+
+const assert = require('assert')
+const _ = require('underscore')
+assert(!_.isUndefined(APP_PORT))
 var http = require('http');
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('Hello World\n');
-}).listen(8080);
-console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
+}).listen(APP_PORT);
+console.log(`Server running at http://APP_PRIVATE_IP_ADDRESS:${APP_PORT}/`);
