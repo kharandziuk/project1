30db94c4ae09f34eb6d02a888421e2fa062b199c
Max Kharandziuk <max.kharandziuk@gmail.com>
2016-02-18 11:11:57 +0200

    some changes

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const {APP_PORT} = process.env
 const assert = require('assert')
 const _ = require('underscore')
 assert(!_.isUndefined(APP_PORT))
-var http = require('http');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Hello World\n');
-}).listen(APP_PORT);
-console.log(`Server running at http://APP_PRIVATE_IP_ADDRESS:${APP_PORT}/`);
+const app = express()
+
+app.get('/test', (req, res) => {
+  res.json({text: 'it works'})
+})
+
+app.listen(APP_PORT, function() {
+  console.log(`listen on port ${APP_PORT}`)
+})
--- src/app/app.jsx
+++ src/app/app.jsx
@@ -3,10 +3,10 @@ require('isomorphic-fetch')
 fetch('/test')
   .then(function(response) {
     if (response.status >= 400) {
-                            throw new Error("Bad response from server");
+      throw new Error("Bad response from server");
                                     }
                       return response.json();
                           })
-    .then(function(stories) {
-              console.log(stories);
+    .then(function({text}) {
+        alert(text)
                   });
